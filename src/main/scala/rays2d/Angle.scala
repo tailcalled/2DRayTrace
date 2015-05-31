3b5792070f8623b63cdb7e991d754af6bce8f244
Kristoffer <kristoffer@ryhl.dk>
2015-05-31 18:09:09 +0200

    fixed angle

--- src/main/scala/rays2d/Angle.scala
+++ src/main/scala/rays2d/Angle.scala
@@ -1,15 +1,17 @@
 package rays2d
 
+import scala.annotation.tailrec
+
 object Angle {
 	def apply(theta: Double) = {
 		new Angle(normalize(theta))
 	}
-	@tailrec def normalize(theta: Double) = {
+	@tailrec def normalize(theta: Double): Double = {
 		val tau = 2*math.Pi
 		if (theta < 0) normalize(theta + tau)
 		else if (theta >= tau) normalize(theta - tau)
 		else theta
 	}
 }
-case class Angle(theta: Double) extends AnyVal
+class Angle private(val theta: Double) extends AnyVal
 
