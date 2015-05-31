a10aab0c59f4b35086be535625ecf4b6c6b1d0fd
Carl Andreas Dybdahl <tailcalled@gmail.com>
2015-05-31 18:47:10 +0200

    Cleaned up some bad code.

--- src/main/scala/rays2d/Ray.scala
+++ src/main/scala/rays2d/Ray.scala
@@ -11,39 +11,45 @@ case class Ray(point: Point, angle: Angle) {
 	def next: Option[(Either[(Ray, Color => Color),Color],Double)] = {
 		val x = point.world.objs.flatMap(x => x.dist(this).map(y => (y,x)))
 		if (x.isEmpty) {
-			return None
+			None
 		}
-		val (dist, obj) = x.minBy(_._1)
-		return Some((obj.reflect(this).get, dist))
+		else {
+			val (dist, obj) = x.minBy(_._1)
+			Some((obj.reflect(this).get, dist))
+		}
 	}
 }
 
 case class XAxis(color: Color) extends WorldPiece {
 	def dist(ray: Ray): Option[Double] = {
 		if (ray.point.x == 0) {
-			return None
+			None
 		}
-		val b = ray.point.y
-		if (b > 0) {
-			if (ray.angle.theta <= math.Pi) return None
-			if (ray.angle.theta < 1.5 * math.Pi) return Some(math.hypot(b, b*math.tan(1.5*math.Pi - ray.angle.theta)))
-			return Some(math.hypot(b, b*math.tan(ray.angle.theta - 1.5 * math.Pi)))
+		else {
+			val b = ray.point.y
+			if (b > 0) {
+				if (ray.angle.theta <= math.Pi) None
+				else if (ray.angle.theta < 1.5 * math.Pi) Some(math.hypot(b, b*math.tan(1.5*math.Pi - ray.angle.theta)))
+				else Some(math.hypot(b, b*math.tan(ray.angle.theta - 1.5 * math.Pi)))
+			}
+			else if (ray.angle.theta >= math.Pi) None
+			else if (ray.angle.theta < 0.5 * math.Pi) Some(math.hypot(b, b*math.tan(0.5*math.Pi - ray.angle.theta)))
+			else Some(math.hypot(b, b*math.tan(ray.angle.theta - 0.5*math.Pi)))
 		}
-		if (ray.angle.theta >= math.Pi) return None
-		if (ray.angle.theta < 0.5 * math.Pi) return Some(math.hypot(b, b*math.tan(0.5*math.Pi - ray.angle.theta)))
-		return Some(math.hypot(b, b*math.tan(ray.angle.theta - 0.5*math.Pi)))
 	}
 	def reflect(ray: Ray): Option[Either[(Ray, Color => Color),Color]] = {
 		if (ray.point.x == 0) {
-			return None
+			None
 		}
-		val b = ray.point.y
-		if (b > 0) {
-			if (ray.angle.theta <= math.Pi) return None
-			return Some(Right(color))
+		else {
+			val b = ray.point.y
+			if (b > 0) {
+				if (ray.angle.theta <= math.Pi) None
+				else Some(Right(color))
+			}
+			else if (ray.angle.theta >= math.Pi) None
+			else Some(Right(color))
 		}
-		if (ray.angle.theta >= math.Pi) return None
-		return Some(Right(color))
 	}
 }
 
