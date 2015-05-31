3edf9997288a058675b5f943de4786dc1489c092
Carl Andreas Dybdahl <tailcalled@gmail.com>
2015-05-31 17:35:53 +0200

    Added keyboard input

--- src/main/scala/rays2d/Engine.scala
+++ src/main/scala/rays2d/Engine.scala
@@ -7,7 +7,25 @@ import javax.swing._
 
 abstract class Engine {
 
-	private var graphics: Graphics2D = null
+	type Key = Int
+
+	private var gfx: Graphics2D = null
+
+	private var _width = 0.0
+	private var _height = 0.0
+
+	private var _keys = Set[Key]()
+	object Keys {
+		import java.awt.event.KeyEvent._
+		val Left = VK_LEFT
+		val Right = VK_RIGHT
+		val Up = VK_UP
+		val Down = VK_DOWN
+	}
+
+	def width = _width
+	def height = _height
+	def pressed = _keys
 
 	def title: String
 
@@ -19,14 +37,27 @@ abstract class Engine {
 			override def paintComponent(gfx: Graphics) = {
 				gfx.drawImage(img, 0, 0, null)
 			}
+			setFocusable(true)
+			import java.awt.event._
+			addKeyListener(new KeyListener() {
+				def keyPressed(ev: KeyEvent) = {
+					_keys += ev.getKeyCode
+				}
+				def keyReleased(ev: KeyEvent) = {
+					_keys -= ev.getKeyCode
+				}
+				def keyTyped(ev: KeyEvent) = {}
+			})
 		}
 		jf.add(comp)
 		jf.setExtendedState(Frame.MAXIMIZED_BOTH)
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE)
 		jf.setVisible(true)
 		while (true) {
-			val img = new BufferedImage(comp.getWidth max 1, comp.getHeight max 1, BufferedImage.TYPE_INT_ARGB)
-			graphics = img.getGraphics.asInstanceOf[Graphics2D]
+			_width = comp.getWidth max 1
+			_height = comp.getHeight max 1
+			val img = new BufferedImage(width.toInt, height.toInt, BufferedImage.TYPE_INT_ARGB)
+			gfx = img.getGraphics.asInstanceOf[Graphics2D]
 			step()
 			comp.img = img
 			comp.repaint()
@@ -35,6 +66,16 @@ abstract class Engine {
 	}
 	def main(args: Array[String]) = {
 		start()
+	}
+
+	private def setColor(col: Color) = gfx.setColor(new java.awt.Color((col.r * 255).round.toInt, (col.g * 255).round.toInt, (col.b * 255).toInt))
+	def drawLine(p1: (Double, Double), p2: (Double, Double), col: Color) = {
+		setColor(col)
+		gfx.drawLine(p1._1.round.toInt, p1._2.round.toInt, p2._1.round.toInt, p2._2.round.toInt)
+	}
+	def drawCircle(p: (Double, Double), rad: Double, col: Color) = {
+		setColor(col)
+		gfx.drawOval((p._1 - rad).round.toInt, (p._2 - rad).round.toInt, (rad * 2).round.toInt, (rad * 2).round.toInt)
 	}
 
 }
--- src/main/scala/rays2d/Test.scala
+++ src/main/scala/rays2d/Test.scala
@@ -4,8 +4,16 @@ object Test extends Engine {
 
 	def title = "2D raytracing test"
 
+	var x = 100.0
+	var y = 100.0
+
 	def step() = {
-		
+		import Keys._
+		if (pressed contains Keys.Left) x -= 4
+		if (pressed contains Keys.Right) x += 4
+		if (pressed contains Keys.Up) y -= 4
+		if (pressed contains Keys.Down) y += 4
+		drawCircle((x, y), 10, Color(0, 0, 0))
 	}
 
 }
